79d313855c2019b06493e6de605b909051f1747d
홍채영 <mgs08041@naver.com>
2023-10-09 18:53:32 +0900

    test6.cpp 작성 완료

--- lab4/test6.cpp
+++ lab4/test6.cpp
@@ -1,4 +1,4 @@
-// #6: UDP로 Protobuf 전송 (완성x)
+// #6: UDP로 Protobuf 전송 (UDP echo server)
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -8,31 +8,61 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
+#include "person.pb.h"
 
 using namespace std;
+using namespace mju;
 
 int main(){
-    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(s<0)return 1;
-
-    string buf = "Hello World";
+    Person *p = new Person;
+    p->set_name("DK Moon");
+    p->set_id(12345678);
     
+    Person::PhoneNumber* phone = p->add_phones();       // 객체 생성 후 반환
+    phone->set_number("010-111-1234");
+    phone->set_type(Person::MOBILE);
+    
+    phone = p->add_phones();
+    phone->set_number("02-100-1000");
+    phone->set_type(Person::HOME);
+    
+    const string s = p->SerializeAsString();
+    cout << "Length:" << s.length() << endl;
+    cout << s << endl << endl;
+
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(sock<0)return 1;
+
+    //string buf = "Hello World";
+
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_port = htons(10001);
     sin.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    int numBytes = sendto(s, buf.c_str(), buf.length(), 0, (struct sockaddr*) &sin, sizeof(sin));
-    cout << "Sent: "<<numBytes<<endl;
+    int numBytes = sendto(sock, s.c_str(), s.length(), 0, (struct sockaddr*) &sin, sizeof(sin));
+    cout << "Sent: " << numBytes << endl;
 
-    char buf2[65536];
+    char s2[65536];
     memset(&sin, 0, sizeof(sin));
     socklen_t sin_size = sizeof(sin);
-    numBytes = recvfrom(s, buf2, sizeof(buf2), 0, (struct sockaddr*) &sin, &sin_size);      // 보낸 사람의 주소를 담을 변수 크기
+    numBytes = recvfrom(sock, s2, sizeof(s2), 0, (struct sockaddr*) &sin, &sin_size);      // 보낸 사람의 주소를 담을 변수 크기
     cout << "Recevied: "<< numBytes << endl;
     cout << "From "<< inet_ntoa(sin.sin_addr) << endl;
 
-    close(s);
+
+    Person *p2 = new Person;        // 객체 생성
+    p2->ParseFromString(s2);
+    cout << "Name:" << p2->name() << endl;
+    cout << "ID:" << p2->id() << endl;
+    for (int i = 0; i < p2->phones_size(); ++i) {
+        cout << "Type:" << p2->phones(i).type() << endl;
+        cout << "Phone:" << p2->phones(i).number() << endl;
+    }
+
+    close(sock);
     return 0;
 }
