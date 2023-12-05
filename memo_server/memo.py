601c8c5b821fa4ba4e6535136d845939151dde16
홍채영 <mgs08041@naver.com>
2023-12-06 07:27:06 +0900

    mysql 서버에 접속한 상태로 localhost:8000 브라우저 접속 가능

--- memo_server/memo.py
+++ memo_server/memo.py
@@ -4,24 +4,25 @@ import requests
 import json
 import urllib
 
-import pymysql
-
 from flask import abort, Flask, make_response, render_template, Response, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 
 naver_client_id = 'SxSmUP2aYM6xCwaKDKGq'
 naver_client_secret = 'Ffz7_NKevs'
 naver_redirect_uri = 'http://localhost:8000/auth'
-'''
-  본인 app 의 것으로 교체할 것.
-  여기 지정된 url 이 http://localhost:8000/auth 처럼 /auth 인 경우
-  아래 onOAuthAuthorizationCodeRedirected() 에 @app.route('/auth') 태깅한 것처럼 해야 함
-'''
 
-db = pymysql.connect(host='localhost', port=3306, user='user', password='0528', db='user_db', charset='utf-8')
+# MySQL 서버에 연결
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:0528@localhost/mysql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = 'user_db'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(255))
+    memo = db.Column(db.Text)
 
 @app.route('/')
 def home():
@@ -36,20 +37,15 @@ def home():
     # TODO: 아래 부분을 채워 넣으시오.
     #       userId 로부터 DB 에서 사용자 이름을 얻어오는 코드를 여기에 작성해야 함
     if userId:
-        query = "SELECT user_name FROM user_db WHERE user_id = %s"
-    
-        with db.cursor() as cursor:
-            cursor.execute(query, (userId,))
-            result = cursor.fetchone()
-            name = result['user_name'] if result else None
+        # SQLAlchemy를 사용하여 데이터베이스에서 사용자 이름 가져오기
+        user = User.query.filter_by(user_id=userId).first()
+        name = user.user_name if user else None
 
     ####################################################
-
 
     # 이제 클라에게 전송해 줄 index.html 을 생성한다.
     # template 로부터 받아와서 name 변수 값만 교체해준다.
     return render_template('index.html', name=name)
-
 
 # 로그인 버튼을 누른 경우 이 API 를 호출한다.
 # OAuth flow 상 브라우저에서 해당 URL 을 바로 호출할 수도 있으나,
@@ -61,16 +57,15 @@ def home():
 # 주의! 아래 API 는 잘 동작하기 때문에 손대지 말 것
 @app.route('/login')
 def onLogin():
-    params={
-            'response_type': 'code',
-            'client_id': naver_client_id,
-            'redirect_uri': naver_redirect_uri,
-            'state': random.randint(0, 10000)
-        }
+    params = {
+        'response_type': 'code',
+        'client_id': naver_client_id,
+        'redirect_uri': naver_redirect_uri,
+        'state': random.randint(0, 10000)
+    }
     urlencoded = urllib.parse.urlencode(params)
     url = f'https://nid.naver.com/oauth2.0/authorize?{urlencoded}'
     return redirect(url)
-
 
 # 아래는 Redirect URI 로 등록된 경우 호출된다.
 # 만일 본인의 Redirect URI 가 http://localhost:8000/auth 의 경우처럼 /auth 대신 다른 것을
@@ -80,17 +75,16 @@ def onOAuthAuthorizationCodeRedirected():
     # TODO: 아래 1 ~ 4 를 채워 넣으시오.
 
     # 1. redirect uri 를 호출한 request 로부터 authorization code 와 state 정보를 얻어낸다.
-    authorization_code = request.args.get('code')       # code: 네이버 로그인 인증에 성공하면 반환받는 인증 코드, 접근 토큰(access token) 발급에 사용
-    state = request.args.get('state')                   # state: 사이트 간 요청 위조 공격을 방지하기 위해 애플리케이션에서 생성한 상태 토큰으로 URL 인코딩을 적용한 값
-
+    authorization_code = request.args.get('code')  # code: 네이버 로그인 인증에 성공하면 반환받는 인증 코드, 접근 토큰(access token) 발급에 사용
+    state = request.args.get('state')  # state: 사이트 간 요청 위조 공격을 방지하기 위해 애플리케이션에서 생성한 상태 토큰으로 URL 인코딩을 적용한 값
 
     # 2. authorization code 로부터 access token 을 얻어내는 네이버 API 를 호출한다.
-    access_token_url = 'https://nid.naver.com/oauth2.0/token'       # 접근 토큰 발급/갱신/삭제 요청
+    access_token_url = 'https://nid.naver.com/oauth2.0/token'  # 접근 토큰 발급/갱신/삭제 요청
     access_token_params = {
-        'grant_type': 'authorization_code',             # 인증 과정에 대한 구분값 - 1. 발급:'authorization_code'
-        'client_id': naver_client_id,                   # 애플리케이션 등록 시 발급받은 Client ID 값
-        'client_secret': naver_client_secret,           # 애플리케이션 등록 시 발급받은 Client secret 값
-        'code': authorization_code,                     # 로그인 인증 요청 API 호출에 성공하고 리턴받은 인증코드값 (authorization_code)
+        'grant_type': 'authorization_code',  # 인증 과정에 대한 구분값 - 1. 발급:'authorization_code'
+        'client_id': naver_client_id,  # 애플리케이션 등록 시 발급받은 Client ID 값
+        'client_secret': naver_client_secret,  # 애플리케이션 등록 시 발급받은 Client secret 값
+        'code': authorization_code,  # 로그인 인증 요청 API 호출에 성공하고 리턴받은 인증코드값 (authorization_code)
         'state': state
     }
 
@@ -106,41 +100,47 @@ def onOAuthAuthorizationCodeRedirected():
         else:
             abort(404, 'Failed to get access token')
 
-        profile_url = 'https://openapi.naver.com/v1/nid/me'     # 유저 프로필 데이터에 접근하는 기본 URL
-        headers = {'Authorization': f'Bearer {access_token}'}   # 얻은 access token을 포함하는 헤더
+        profile_url = 'https://openapi.naver.com/v1/nid/me'  # 유저 프로필 데이터에 접근하는 기본 URL
+        headers = {'Authorization': f'Bearer {access_token}'}  # 얻은 access token을 포함하는 헤더
 
-        profile_response = requests.get(profile_url, headers=headers)       # 프로필 정보를 가져옴
+        profile_response = requests.get(profile_url, headers=headers)  # 프로필 정보를 가져옴
 
         if profile_response.status_code == 200:
             user_info = profile_response.json().get('response', {})
             user_id = user_info.get('id')
             user_name = user_info.get('name')
-            
-            # 4. 얻어낸 user id 와 name 을 DB 에 저장한다.
-            user_id = None
-            user_name = None
 
+            # 4. 얻어낸 user id 와 name 을 DB 에 저장한다.
             if user_id and user_name:
                 try:
-                    with db.cursor() as cursor:
-                        query = "INSERT INTO user_db (user_id, user_name) VALUES (%s, %s)"
-                        cursor.execute(query, (user_id, user_name))
-                    db.commit()
+                    # SQLAlchemy를 사용하여 데이터베이스에 유저 정보 저장
+                    user = User.query.filter_by(user_id=user_id).first()
+                    if user:
+                        user.user_name = user_name
+                    else:
+                        new_user = User(user_id=user_id, user_name=user_name)
+                        db.session.add(new_user)
+                    db.session.commit()
                 except Exception as e:
                     print(f"Error: while saving user info to DB: {e}")
-                    db.rollback()
+                    db.session.rollback()
 
-            # 5. 첫 페이지로 redirect 하는데 로그인 쿠키를 설정하고 보내준다.
-            response = redirect('/')
-            response.set_cookie('userId', user_id)
-            return response
+                # 5. 첫 페이지로 redirect 하는데 로그인 쿠키를 설정하고 보내준다.
+                response = redirect('/')
+                response.set_cookie('userId', str(user_id))  # +
+                print(response)
+                print("성공!!")
+                print(user_id)
+                return response
+            else:
+                # user_id 또는 user_name이 없는 경우
+                abort(404, 'Failed to get user id or user name')
         else:
             # 프로필 정보를 가져오는데 실패한 경우
             abort(profile_response.status_code)
     else:
         # Access Token을 가져오는데 실패한 경우
         abort(response.status_code)
-        
 
 
 @app.route('/memo', methods=['GET'])
@@ -154,8 +154,16 @@ def get_memos():
     # + 이 함수의 return 형태대로 리턴하도록 수정
     result = []
 
-    # memos라는 키 값으로 메모 목록 보내주기
-    return {'memos': result}
+    try:
+        # SQLAlchemy를 사용하여 데이터베이스에서 메모들을 가져오기
+        user = User.query.filter_by(user_id=userId).first()
+        memos = user.memo.split('\n') if user and user.memo else []
+        result = [{'content': memo} for memo in memos]
+    except Exception as e:
+        print(f"Error while fetching memos: {e}")
+
+    # # memos라는 키 값으로 메모 목록 보내주기
+    return Response(json.dumps({'memos': result}), content_type='application/json')
 
 
 @app.route('/memo', methods=['POST'])
@@ -170,10 +178,20 @@ def post_new_memo():
         abort(HTTPStatus.BAD_REQUEST)
 
     # TODO: 클라이언트로부터 받은 JSON 에서 메모 내용을 추출한 후 DB에 userId 의 메모로 추가한다.
+    try:
+        memo_content = request.json.get('text', '')
+        # SQLAlchemy를 사용하여 데이터베이스에 새로운 메모 추가
+        user = User.query.filter_by(user_id=userId).first()
+        if user:
+            user.memo = (user.memo or "") + memo_content + '\n'
+            db.session.commit()
+    except Exception as e:
+        print(f"Error while saving memo: {e}")
+        db.session.rollback()
+        return Response(json.dumps({'error': 'Failed to save memo'}), content_type='application/json'), HTTPStatus.INTERNAL_SERVER_ERROR
 
     #
     return '', HTTPStatus.OK
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000, debug=True)
